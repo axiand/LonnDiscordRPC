b2d7a3c6bf5e8c98a0c01e43ff14354cec4c138a
Axi <98420477+axiand@users.noreply.github.com>
2024-01-23 12:45:29 +0100

    check if file already exists before setup

--- Loenn/libraries/Installer.lua
+++ Loenn/libraries/Installer.lua
@@ -6,6 +6,7 @@
 local fs = require("utils.filesystem")
 local locations = require("file_locations")
 local mods = require("mods")
+local logger = require("logging")
 
 local os = love.system.getOS()
 
@@ -15,5 +16,10 @@ local DLL_SOURCE_URL = "https://raw.githubusercontent.com/axiand/LonnDiscordRPC/
 
 local targetPathSplit = fs.splitpath(locations.getSourcePath())
 table.remove(targetPathSplit, #targetPathSplit) -- remove last item because its the executable file
+
+if fs.isFile(fs.joinpath(fs.joinpath(targetPathSplit), "discord-rpc.dll")) then
+    logger.info("RPC binary already exists, skipping setup")
+    return
+end
 
 fs.downloadURL(DLL_SOURCE_URL, fs.joinpath(fs.joinpath(targetPathSplit), "discord-rpc.dll"))
